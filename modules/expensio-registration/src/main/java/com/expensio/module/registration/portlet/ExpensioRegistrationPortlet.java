d6e691baa21a68aab60eb7ffee16a6a1cab89f3c
Keval Kathiriya <kevalkathiriya172@gmail.com>
2024-04-02 18:40:58 +0530

    save registration module with only register employee

---
+++ modules/expensio-registration/src/main/java/com/expensio/module/registration/portlet/ExpensioRegistrationPortlet.java
@@ -0,0 +1,37 @@
+package com.expensio.module.registration.portlet;
+
+import com.expensio.module.registration.constants.ExpensioRegistrationPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author keval.kathiriya
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=ExpensioRegistration",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ExpensioRegistrationPortletKeys.EXPENSIOREGISTRATION,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ExpensioRegistrationPortlet extends MVCPortlet {
+	
+	
+}
---
+++ modules/expensio-registration/src/main/resources/META-INF/resources/html/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="expensioregistration.caption"/></b>
+</p>
