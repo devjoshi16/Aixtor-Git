59d28e8e67b2d78775bed8eda794397e054dbdea
Nivid Koradiya <ndkoradiya@gmail.com>
2024-04-03 14:50:21 +0530

    Created new module: department-web

---
+++ modules/expensio-department-web/src/main/java/com/expensio/department/portlet/ExpensioDepartmentPortlet.java
@@ -0,0 +1,29 @@
+package com.expensio.department.portlet;
+
+import com.expensio.department.constants.ExpensioDepartmentPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author nivid.koradiya
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=ExpensioDepartment",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ExpensioDepartmentPortletKeys.EXPENSIODEPARTMENT,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ExpensioDepartmentPortlet extends MVCPortlet {
+}
